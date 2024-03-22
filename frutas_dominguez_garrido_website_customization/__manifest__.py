0d91cad6fd90cfd3d610982a417a6b6905a452ee
GonzalezJimenezSalvador <salvador.gon.jim@gmail.com>
2024-03-22 10:25:28 +0100

    Add remove prices

---
+++ frutas_dominguez_garrido_website_customization/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Frutas Dominguez Garrido",
+    "summary": "Website customization",
+    "version": "16.0.1.0.0",
+    "category": "Website",
+    "author": "Manuel Calero, Salvador Gonzalez,Abraham Carrasco, Xtendoo",
+    "license": "LGPL-3",
+    "application": True,
+    "depends": [
+        "website_sale",
+    ],
+    "data": [
+        "views/website_customization_view.xml",
+    ],
+    "installable": True,
+}
